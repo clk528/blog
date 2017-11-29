0603fd9c94b06fb5e4bed54c068cf60023f8b899
undefined <chenlongke@8jpocket.com>
2017-11-29 18:07:25 +0800

    添加help文件

---
+++ helpers/Commom.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: clk528
+ * Date: 2017-11-29
+ * Time: 17:45
+ */
+
+if(!function_exists('password_decode'))
+{
+    /**
+     * 公钥加密
+     * @param $string
+     * @param $publicKey
+     * @return string
+     */
+    function password_decode($string,$publicKey)
+    {
+        $data = "";
+        $dataArray = str_split($string, 117);
+        foreach ($dataArray as $value) {
+            $encryptedTemp = "";
+            openssl_public_encrypt($value,$encryptedTemp,$publicKey);//公钥加密
+            $data .= base64_encode($encryptedTemp);
+        }
+        return $data;
+    }
+}
+
+if(!function_exists('password_encode'))
+{
+    /**
+     * 私钥解密
+     * @param $string
+     * @param $privateKey
+     * @return string
+     */
+    function password_encode($string,$privateKey)
+    {
+        $decrypted = "";
+        $decodeStr = base64_decode($string);
+        $enArray = str_split($decodeStr, 256);
+
+        foreach ($enArray as $va) {
+            openssl_private_decrypt($va,$decryptedTemp,$privateKey);//私钥解密
+            $decrypted .= $decryptedTemp;
+        }
+        return $decrypted;
+    }
+}
