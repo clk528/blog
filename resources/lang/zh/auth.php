269693be4a44e9e46ab393d03c37d7f965f1815d
clk528 <clk528@qq.com>
2017-12-01 18:43:43 +0800

    添加中文翻译

---
+++ resources/lang/zh/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '该用户在系统中不存在.',
+    'throttle' => '登录尝试次数太多。请等待:seconds秒后再试.',
+
+];
---
+++ resources/lang/zh/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须至少有六个字符，并与确认相同.',
+    'reset' => '您的密码已重置!',
+    'sent' => '我们已经通过电子邮件向你发送了重置链接！',
+    'token' => '密码重置的token无效.',
+    'user' => "我们找不到这个电子邮件地址的用户.",
+
+];
