2dc33466378fc4ffc8884d05bec4953f7020c4ef
clk528 <clk528@qq.com>
2017-12-07 17:33:09 +0800

    修改斜杠不兼容的问题

--- database/seeds/create_default_config_seeder.php
+++ database/seeds/create_default_config_seeder.php
@@ -34,8 +34,8 @@ class create_default_config_seeder extends Seeder
         $article1 = \App\Entities\Article::create([
             'title' => 'test',
             'subtitle' => '测试子标题',
-            'markdown' => file_get_contents(base_path()."\\tests\\test.md"),
-            'html' => file_get_contents(base_path()."\\tests\\test.html"),
+            'markdown' => file_get_contents(base_path()."/tests/test.md"),
+            'html' => file_get_contents(base_path()."/tests/test.html"),
             'tag' => 'test',
             'create_user' => $user->create_user,
             'modify_user' => $user->create_user,
@@ -54,8 +54,8 @@ class create_default_config_seeder extends Seeder
         $article2 = \App\Entities\Article::create([
             'title' => '恢复git reset后的commit',
             'subtitle' => 'git 恢复',
-            'markdown' => file_get_contents(base_path()."\\tests\\test2.md"),
-            'html' => file_get_contents(base_path()."\\tests\\test2.html"),
+            'markdown' => file_get_contents(base_path()."/tests/test2.md"),
+            'html' => file_get_contents(base_path()."/tests/test2.html"),
             'tag' => 'git',
             'create_user' => $user->create_user,
             'modify_user' => $user->create_user,
